6ddc1186788c36f1644799cb7883c974f68d3f1f
hefeixiong <1074535830@qq.com>
2021-08-16 23:35:41 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode : 'development' ,
+    entry : './src/index.js',
+    output : {
+        filename : '[name].[contenthash].js'
+    },
+    plugins : [new HtmlWebpackPlugin()]
+};
