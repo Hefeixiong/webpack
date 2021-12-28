9a10c893cb18642ffada33d31e6ef4de8788f1d4
hefeixiong <1074535830@qq.com>
2021-12-28 20:38:08 +0800

    v1

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,17 @@ module.exports = {
     mode : 'development' ,
     entry : './src/index.js',
     output : {
-        filename : '[name].[contenthash].js'
+        filename : 'index.[contenthash].js'
     },
-    plugins : [new HtmlWebpackPlugin()]
-};
+    plugins : [new HtmlWebpackPlugin({
+        title : 'My html'
+    })],
+    module : {
+        rules : [
+            {
+                test : /\.css$/i,
+                use : ['style-loader' , 'css-loader'],
+            },
+        ],
+    },
+};  
